a9f2150a85459f81cd3b13da0f20509e4da1f5dc
christopher <christopher.harold.butler@gmail.com>
2022-12-04 12:05:12 +0100

    adding int array

--- CodeExercises/Program.cs
+++ CodeExercises/Program.cs
@@ -1,16 +1,26 @@
-﻿using CodeExercises.Exercises;
+﻿using CodeExercises.DataStructureTests;
+using CodeExercises.Exercises;
 
 namespace CodeExercises;
+
 class Program
 {
     static void Main(string[] args)
     {
-        for (int i = 0; i < 20; i++)
-        {
-            Console.WriteLine(FizzBuzz.Run(i));
-        }
+        
 
         Console.Read();
+    }
+
+    private void ExerciseArray()
+    {
+        CustomArray numbers = new CustomArray(3);
+        numbers.Insert(10);
+        numbers.Insert(20);
+        numbers.Insert(30);
+        numbers.Insert(40);
+        numbers.removeAt(0);
+        numbers.print();
     }
 }
 
