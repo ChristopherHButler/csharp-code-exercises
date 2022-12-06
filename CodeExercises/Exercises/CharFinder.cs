ee3ab7ff190312540527b17f68b3856ab6ec487d
christopher <christopher.harold.butler@gmail.com>
2022-12-06 14:38:59 +0100

    adding hash table / dictionary exercises

---
+++ CodeExercises/Exercises/CharFinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+namespace CodeExercises.Exercises;
+
+public class CharFinder
+{
+    public static char FindFirstNonRepeatingChar(string str)
+    {
+        Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+        var chars = str.ToCharArray();
+
+        foreach (var ch in chars)
+        {
+            var count = dictionary.ContainsKey(ch) ? dictionary[ch] : 0;
+            dictionary.Add(ch, count + 1);
+        }
+
+        foreach (var ch in chars)
+        {
+            if (dictionary[ch] == 1)
+                return ch;
+        }
+        return Char.MinValue;
+    }
+}
+
---
+++ CodeExercises/Exercises/Dictionary.cs
@@ -0,0 +1,82 @@
+﻿using System;
+namespace CodeExercises.Exercises;
+
+public class Dictionary
+{
+    private class Entry
+    {
+        public int Key { get; set; }
+        public string Value { get; set; }
+
+        public Entry(int key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+
+    private LinkedList<Entry>[] entries = new LinkedList<Entry>[5];
+
+    public string Get(int key)
+    {
+        var index = hash(key);
+        var bucket = entries[index];
+        if (bucket != null)
+        {
+            foreach (var entry in bucket)
+            {
+                if (entry.Key == key)
+                    return entry.Value;
+            
+            }
+        }
+        return string.Empty;
+    }
+
+    public void Put(int key, string value)
+    {
+        var index = hash(key);
+        var bucket = entries[index];
+
+        if (bucket == null)
+            bucket = new LinkedList<Entry>();
+
+        foreach (var entry in bucket)
+        {
+            if (entry.Key == key)
+            {
+                entry.Value = value;
+                return;
+            }
+        }
+
+        bucket.AddLast(new Entry(key, value));
+    }
+
+    public void Remove(int key)
+    {
+        var index = hash(key);
+        var bucket = entries[index];
+
+        if (bucket == null)
+            throw new Exception();
+
+        foreach (var entry in bucket)
+        {
+            if (entry.Key == key)
+            {
+                bucket.Remove(entry);
+                return;
+            }
+        }
+        throw new Exception();
+    }
+
+    private int hash(int key)
+    {
+        return Math.Abs(key) % entries.Length;
+    }
+
+    
+}
+
